8264b7c028817757a50e2da8b636d40b2c861833
Ioannis Canellos <iocanel@gmail.com>
2023-11-27 11:03:01 +0200

    chore: use matching packages in Codegen

--- model/export/src/main/java/io/quarkus/kubevirt/codegen/Codegen.java
+++ model/export/src/main/java/io/quarkus/kubevirt/codegen/Codegen.java
@@ -7,8 +7,8 @@ import io.sundr.transform.annotations.TemplateTransformations;
 @TemplateTransformations(annotations = @AnnotationSelector(io.fabric8.kubernetes.model.annotation.Group.class), value = {
         @TemplateTransformation("/resource-list.vm"),
         @TemplateTransformation(value = "/service.vm", outputPath = "META-INF/services/io.fabric8.kubernetes.api.model.KubernetesResource", gather = true),
-        @TemplateTransformation(value = "/dsl.vm", outputPath = "io/quarkus/kubevirt/v1/Dsl.java", gather = true),
-        @TemplateTransformation(value = "/client.vm", outputPath = "io/quarkus/kubevirt/v1/Client.java", gather = true)
+        @TemplateTransformation(value = "/dsl.vm", outputPath = "io/quarkus/kubevirt/export/v1alpha1/Dsl.java", gather = true),
+        @TemplateTransformation(value = "/client.vm", outputPath = "io/quarkus/kubevirt/export/v1alpha1/Client.java", gather = true)
 })
 public class Codegen {
 }
--- model/v1alpha3/src/main/java/io/quarkus/kubevirt/codegen/Codegen.java
+++ model/v1alpha3/src/main/java/io/quarkus/kubevirt/codegen/Codegen.java
@@ -6,7 +6,9 @@ import io.sundr.transform.annotations.TemplateTransformations;
 
 @TemplateTransformations(annotations = @AnnotationSelector(io.fabric8.kubernetes.model.annotation.Group.class), value = {
         @TemplateTransformation("/resource-list.vm"),
-        @TemplateTransformation(value = "/service.vm", outputPath = "META-INF/services/io.fabric8.kubernetes.api.model.KubernetesResource", gather = true)
+        @TemplateTransformation(value = "/service.vm", outputPath = "META-INF/services/io.fabric8.kubernetes.api.model.KubernetesResource", gather = true),
+        @TemplateTransformation(value = "/dsl.vm", outputPath = "io/quarkus/kubevirt/v1alpha3/Dsl.java", gather = true),
+        @TemplateTransformation(value = "/client.vm", outputPath = "io/quarkus/kubevirt/v1alpha3/Client.java", gather = true)
 })
 public class Codegen {
 }
